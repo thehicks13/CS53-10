378223dd645d05c00f6cbc165e8277eb4b3d24f8
Tyler Jenkins <Jenks0002@gmail.com>
2013-11-18 16:30:05 -0600

    Update customer.hpp

--- customer.hpp
+++ customer.hpp
@@ -4,3 +4,30 @@
 //CLASS: CS 53
 //SECTION: E
 //DATE: 18 November, 2013
+
+#include "customer.h"
+
+bool Customer::purchase(string item)
+{
+  bool pSuccess=false;
+  if(m_numPurch < 20)
+  {
+    m_numPurch++;
+    m_purchases[m_numPurch]=item;
+    pSuccess=true;
+  }
+  return pSuccess;
+}
+
+void Customer::print()
+{
+  cout<<m_cName<<" has $"<<m_cash<<" and purchases ";
+  for(int j=0;j<=m_numPurch;j++)
+  {
+    if(j==m_numPurch)
+      cout<<m_purchases[j]<<"."<<endl;
+    else
+      cout<<m_purchases[j]<<", ";
+  }
+  return;
+}
