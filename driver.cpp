a2cfe4431ad582d584b6ddeba54bdb760e08fd4f
Tyler Jenkins <Jenks0002@gmail.com>
2013-11-18 16:33:10 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -4,3 +4,35 @@
 //CLASS: CS 53
 //SECTION: E
 //DATE: 18 November, 2013
+
+#include <iostream>
+#include "customer.h"
+#include "business.h"
+using namespace std;
+
+int main()
+{
+  srand(time(NULL));
+  string name1 = "Michael";
+  string name2 = "Emma";
+  string name3 = "Aaron";
+  string bname1 = "Tyler's 5 & Dime";
+
+  Customer SusanD(name1,SusanD.getMoney());
+  Customer TuckerW(name2,TuckerW.getMoney());
+  Customer AaronR(name3,AaronR.getMoney());
+
+  Business BigBadCars(bname1,BigBadCars.getMoney());
+
+  BigBadCars.addCustomer(SusanD);
+  BigBadCars.addCustomer(TuckerW);
+  BigBadCars.addCustomer(AaronR);
+
+  BigBadCars.make_a_sale();
+  BigBadCars.make_a_sale();
+  BigBadCars.make_a_sale();
+
+  BigBadCars.print();
+
+  return 0;
+}
