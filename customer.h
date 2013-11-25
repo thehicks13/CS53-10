1963a0e08521832dec9a356d17c47e4a8950ac74
thehicks13 <thehicks13@yahoo.com>
2013-11-25 17:00:37 -0600

    Update customer.h

--- customer.h
+++ customer.h
@@ -59,7 +59,7 @@ class Customer
   public:
     Customer();
     template <typename T>
-    bool buy_something(T& cust, T& Corp);
+    bool buy_something(T& cust, const T& Corp);
     string getName(){return m_cName;}
     float getMoney(){return m_cash;}
     void setMoney(const float cash){m_cash=cash; return;}
