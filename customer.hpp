8b3fd431a1fd25de8dfe73fe4845498182256e7d
thehicks13 <thehicks13@yahoo.com>
2013-11-27 16:54:27 -0600

    Update customer.hpp

--- customer.hpp
+++ customer.hpp
@@ -31,6 +31,7 @@ bool Customer::buy_something(const T& Corp)
       {
          product = rand()%Corp.m_numMerch;
          m_purchases[cust.m_numPurch]=Corp.m_merch[product];
+         cust.m_cash -= aaaaaaaaaaaaaaaaaaa;
          m_happiness += 25;
          m_numPurch++;
          pSuccess=true;
