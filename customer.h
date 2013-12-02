6a38f71cb2a544e66c11012ec78ceba74deeed81
Tyler Jenkins <Jenks0002@gmail.com>
2013-12-02 16:51:21 -0600

    Update customer.h

--- customer.h
+++ customer.h
@@ -19,27 +19,21 @@ const int NIRVANA = 100;
 const int MAX_CASH = 101;
 const int NUM_CUST = 20;
 /*
-~~purchase()~~
-DESC: adds an item to a customer's purchases if they have room for it
-PRE:  none
-POST: returns a bool, depending on whether or not the person had room for
-      another item
-~~getName()~~
-DESC: makes the name available for use outside the class
-PRE:  none
-POST: returns the string of m_cName
-~~getMoney()~~
-DESC: makes the money available for use outside the class
-PRE:  none
-POST: returns the value of m_cash
-~~setMoney()~~
-DESC: sets m_cash equal to the value sent to it
-PRE:  none
-POST: returns nothing, but changes the value of m_cash
-~~print()~~
-DESC: prints out details of a customer to the screen
-PRE:  none
-POST: returns nothing, but prints to the screen
+~~void set_name(Customer peoples)~~
+Desc: Sets the name and inclination of all the customers in the array
+Pre: an array of customers must e initialized
+Post: the array of customers now have names an inclination to a store
+
+~~ template <typename T>
+  bool buy_something(const T& Corp)~~
+Desc: Decides whether or not the customer buys something or is able to
+Pre: the customer's funds must be set
+Post: If the customer was able to buy something it's happiness went up,
+      if not, it went down
+      
+~~void throwItem(customer cust, int perp, int victim)~~
+Desc: 
+
 */
 
 struct product
@@ -63,7 +57,6 @@ class Customer
     template <typename T>
     bool buy_something(const T& Corp);
     void throwItem(customer cust, int perp, int victim);
-    void shelbyville();
     void rob(customer cust, int perp);
     void action(customer cust, int perp, int victim);
     friend ostream& operator <<(ostream& os, const Customer cust);
